c6cf0aa25e8fc74d26a759ad896e66468562f390
Anne FOUGERE <afougere@e1r2p20.42.fr>
2018-01-13 09:24:40 +0100

    Creation of the project

---
+++ rush00/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rush00
+//
+//  Created by Anne FOUGERE on 1/13/18.
+//  Copyright © 2018 Anne FOUGERE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
